bd270bea470b9371fcc262914dab6fd99bd4893c
GeoTsinis <geo.tsinis@gmail.com>
2023-04-28 17:43:30 +0300

    + More Info Button + InfoModal popup + MovieCard moreInfo button

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -33,7 +33,7 @@ const InfoModal: React.FC<InfoModalProps> = ({ visible, onClose }) => {
   }
 
   return (
-    <div className="z-50 transition duration-300 bg-black bgg-opacity-80 flex justify-xenter items-center overflow-x-hidden overflow-y-auto fixed inset-0">
+    <div className="z-50 transition duration-300 bg-black bg-opacity-80 flex justify-xenter items-center overflow-x-hidden overflow-y-auto fixed inset-0">
       <div className="relative w-auto mx-auto max-w-3xl rounded-md overflow-hidden">
         <div
           className={`${
